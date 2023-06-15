5bd755ea4a3254707b9ab3fe036bde78ed33fcfa
Dominik Toth <dominik8655@gmail.com>
2023-06-15 19:33:23 +0200

    feat: task2

--- src/task1.py
+++ src/task1.py
@@ -1,28 +1,31 @@
-from turtle import *
+import turtle as t
 from random import randint
 
+
 def obdlznik(a: int, b: int):
-    pendown()
+    t.pendown()
     for i in range(4):
-        forward(a if i % 2 == 0 else b)
-        right(90)
-    penup()
+        t.forward(a if i % 2 == 0 else b)
+        t.right(90)
+    t.penup()
+
 
 def slnko(n: int, strana: int, luc: int):
-    goto(randint(-350, 300), randint(100, 220))
+    t.goto(randint(-350, 300), randint(100, 220))
     if (n < 5 or n > 15) or (strana < 10 or strana > 30) or (luc < 30 or luc > 80):
-        raise "Incorrect parameters"
+        raise Exception("Incorrect parameters")
 
     for _ in range(n):
         obdlznik(luc, strana)
-        right(90)
-        forward(strana)
-        left(270 + ((n-2)*180)/n)
+        t.right(90)
+        t.forward(strana)
+        t.left(270 + ((n - 2) * 180) / n)
 
-penup()
-seth(270)
+
+t.penup()
+t.seth(270)
 slnko(7, 13, 33)
 
-color('black', 'red')
+t.color("black", "red")
 
-done()
+t.done()
--- src/task2.py
+++ src/task2.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import os
-import re as regex
 import random
 
 WIDTH = 800
@@ -9,34 +8,54 @@ HEIGHT = 600
 canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-def click(number):
-    print(number)
 
-image_files = os.listdir('assets')
-animals = list(filter(lambda file: file.startswith('zviera'), image_files))
-steps = list(filter(lambda file: file.startswith('stopy'), image_files))
+image_files = os.listdir(f"{os.getcwd()}/assets")
+
+animals = list(filter(lambda file: file.startswith("zviera"), image_files))
+# randomize animals list so that they are not in the same order all the time
+random.shuffle(animals)
+
+steps = list(filter(lambda file: file.startswith("stopy"), image_files))
 # select random step
 step = random.choice(steps)
+photo_images = {"animals": [], "step": None}
 
-photo_images = {'animals': [], 'step': None}
-for idx, img in enumerate(animals):
-    number = regex.findall(r'\d+', img)[0]
-    # button = tk.Button(borderwidth=0, highlightthickness=0)
-    # button.pack()
-    img = tk.PhotoImage(file=f"assets/{img}")
-    photo_images['animals'].append(img)
-    # button.bind('<Button-1>', func=click)
 
-    label = tk.Label(image=img, borderwidth=0, highlightthickness=0)
-    label.place(x=100+idx*50, y=600, anchor='center')
-    label.pack()
-    label.bind('<Button-1>', func=lambda x=number: click(number))
+def move_to_coords(tag: str, x: int, y: int):
+    coords = canvas.coords(tag)
+    canvas.move(tag, x - coords[0], y - coords[1])
 
-    # button.config(image=img, anchor='center')
-    # canvas.create_image(100+idx*150, 100, anchor='center', image=photo_images['animals'][-1], tag=f"zviera-{number}")
 
-photo_images['step'] = tk.PhotoImage(file=f"assets/{step}")
+def click(number: int):
+    # check if clicked animal belongs to steps
+    if str(number) not in step:
+        return
+
+    # move animal to steps
+    move_to_coords(f"animal-{number}", 100, 500)
+    canvas.update()
+    # while animal is on screen move by 50px to the right
+    i = 0
+    while canvas.coords(f"animal-{number}")[0] < WIDTH + 100:
+        i += 1
+        canvas.after(500)
+        canvas.move(f"animal-{number}", 100, 0)
+        canvas.create_image(100 + i * 100, 500, anchor="center", image=photo_images["step"])
+        canvas.update()
+
+
+for idx, file_name in enumerate(animals):
+    number = file_name[-5]
+    img = tk.PhotoImage(file=f"./assets/{file_name}")
+    photo_images["animals"].append(img)
+
+    # draw image
+    canvas.create_image(100 + idx * 150, 150, anchor="s", image=photo_images["animals"][-1], tag=f"animal-{number}")
+    # add event listener
+    canvas.tag_bind(f"animal-{number}", "<Button-1>", func=lambda _, number=number: click(number))
+
+photo_images["step"] = tk.PhotoImage(file=f"./assets/{step}")
 number = step[-5]
-canvas.create_image(100, 500, anchor='center', image=photo_images['step'], tag=f"step-{number}")
+canvas.create_image(100, 500, anchor="center", image=photo_images["step"], tag=f"step-{number}")
 
 tk.mainloop()
